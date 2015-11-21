bd2640d7e9322d961841194e211f4dd0706fdae2
akazuko <sharma.ayush1995@google.com>
2015-11-21 18:42:12 +0530

    Cache update - comparator added

--- Cache/cacheModule.v
+++ Cache/cacheModule.v
@@ -4,6 +4,8 @@ module cacheModule(input clk, input reset, input [31:0] pcOut,
 	wire [7:0] decOut8b;
 	decoder3to8 dec0(pcOut[7:5],  decOut8b);
 
+	// ..........Comparators
+
 	wire tag0_set0_equal, tag1_set0_equal, tag2_set0_equal, tag3_set0_equal;
 	wire tag4_set0_equal, tag5_set0_equal, tag6_set0_equal, tag7_set0_equal;
 	wire tag0_set1_equal, tag1_set1_equal, tag2_set1_equal, tag3_set1_equal;
@@ -28,6 +30,8 @@ module cacheModule(input clk, input reset, input [31:0] pcOut,
 	Comparator C5_set1(pcOut[11:8], tag5_set0_H, tag5_set1_equal);
 	Comparator C6_set1(pcOut[11:8], tag6_set0_H, tag6_set1_equal);
 	Comparator C7_set1(pcOut[11:8], tag7_set0_H, tag7_set1_equal);
+
+	// ........ Cache Set
 
 	// Take the input data ( 32B ) from the memory
 	// The input to the tag for the set is same for all
@@ -60,6 +64,8 @@ module cacheModule(input clk, input reset, input [31:0] pcOut,
 					tag0_set1_H, tag1_set1_H, tag2_set1_H, tag3_set1_H, tag4_set1_H, tag5_set1_H, tag6_set1_H, tag7_set1_H,
 					tag_set1_M, outData_set1);
 	
+	// .......... Halt_tag Selection
 
+	or OR_HALT_TAG_MATCHING()
 
 endmodule
